0a9dde7355c6e34361631b732433d5897c2eac62
FVS-MBA <feliven@gmail.com>
2026-02-27 19:32:58 -0300

    substitui RouterTestingModule por RouterModule

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,11 @@
+import { RouterModule } from '@angular/router';
 import { TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
 import { App } from "./app";
 
 describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [RouterTestingModule, App],
+    imports: [App, RouterModule],
 }).compileComponents();
   });
 
