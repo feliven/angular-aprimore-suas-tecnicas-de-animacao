7364654c437e7fe7b41d54798a7e2f02709318f4
FVS <feliven@gmail.com>
2026-03-05 13:34:21 -0300

    muda shakeAnimation para ser ativada se campo for tocado e estiver inválido

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -78,31 +78,23 @@ export const formButtonTrigger = trigger("formButton", [
 ]);
 
 export const shakeTrigger = trigger("shakeAnimation", [
-  transition("* => *", [
-    query(
-      "input.ng-invalid:focus, select.ng-invalid:focus",
-      [
-        animate(
-          "0.75s",
-          keyframes([
-            style({ border: "2px solid red" }),
-            style({ transform: "translateX(0)" }),
-            style({ transform: "translateX(-15px)" }),
-            style({ transform: "translateX(15px)" }),
-            style({ transform: "translateX(-15px)" }),
-            style({ transform: "translateX(15px)" }),
-            style({ transform: "translateX(-15px)" }),
-            style({ transform: "translateX(15px)" }),
-            style({ transform: "translateX(-15px)" }),
-            style({ transform: "translateX(15px)" }),
-            style({ transform: "translateX(-15px)" }),
-            style({ transform: "translateX(0)" }),
-          ]),
-        ),
-      ],
-      {
-        optional: true,
-      },
+  transition("false => true", [
+    animate(
+      "0.75s",
+      keyframes([
+        style({ border: "2px solid red" }),
+        style({ transform: "translateX(0)" }),
+        style({ transform: "translateX(-15px)" }),
+        style({ transform: "translateX(15px)" }),
+        style({ transform: "translateX(-15px)" }),
+        style({ transform: "translateX(15px)" }),
+        style({ transform: "translateX(-15px)" }),
+        style({ transform: "translateX(15px)" }),
+        style({ transform: "translateX(-15px)" }),
+        style({ transform: "translateX(15px)" }),
+        style({ transform: "translateX(-15px)" }),
+        style({ transform: "translateX(0)" }),
+      ]),
     ),
   ]),
 ]);
