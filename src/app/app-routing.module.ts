648aeb222177098aa5f3970cabec1433eb9e0237
FVS <feliven@gmail.com>
2026-03-05 18:22:21 -0300

    inicia refatoração para remover ReuseStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,7 @@
 import { NgModule } from "@angular/core";
-import { RouteReuseStrategy, RouterModule, Routes } from "@angular/router";
-import { AppRouteReuseStrategy } from "./app-route-reuse-strategy";
-
-import { ListaTarefas } from "./pages/lista-tarefas/lista-tarefas";
-
-// const routes: Routes = [
-//   {
-//     path: "",
-//     redirectTo: "listaTarefas",
-//     pathMatch: "full",
-//     data: {
-//       reuseComponent: true,
-//     },
-//   },
-//   {
-//     path: "listaTarefas",
-//     component: ListaTarefas,
-//     data: {
-//       reuseComponent: true,
-//     },
-//   },
-// ];
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: "reload" })],
   exports: [RouterModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: AppRouteReuseStrategy }],
 })
 export class AppRoutingModule {}
